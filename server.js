e7faf26b362d3a510ec19a0ecf0d117ddf014fc7
yuumisa <skywon@live.unc.edu>
2022-03-09 20:29:36 -0500

    added coin functions

--- server.js
+++ server.js
@@ -11,17 +11,33 @@ const server = app.listen(port, () => {
 });
 
 app.get('/app/', (req, res) => {
-        res.statusCode = 200;
-        res.statusMessage = 'OK';
-        res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
-        res.end(res.statusCode+ ' ' +res.statusMessage)
-    });
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+    res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
+    res.end(res.statusCode+ ' ' +res.statusMessage)
+});
 
-    app.get('/app/flips/:number', (req, res) => {
-        res.statusCode = 200;
-        const flips = req.params.number;
-        res.json({"raw":coinFlips(flips), "summary":countFlips(flips)})
-    });
+app.get('/app/flips/:number', (req, res) => {
+    res.statusCode = 200;
+    const flips = req.params.number;
+    res.json({"raw":coinFlips(flips), "summary":countFlips(flips)})
+});
+
+app.get('/app/flip/', (req, res) => {
+    res.statusCode = 200;
+    res.json({"flip": coinFlip()})
+});
+
+
+app.get('/app/flip/call/heads', (req, res) => {
+    res.statusCode = 200;
+    res.json(flipACoin("heads"))
+});
+
+app.get('/app/flip/call/tails', (req, res) => {
+    res.statusCode = 200;
+    res.json(flipACoin("tails"))
+});
 
 app.use(function(req, res){
     res.status(404).send('404 NOT FOUND')
